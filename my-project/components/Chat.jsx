d4bc974e5f4b8121dd13ac1f71f0be5e1b89c454
Asif-shaikh-dev <asifsk.5070@gmail.com>
2025-07-22 19:02:56 +0530

    feat: update Chat component layout and styling

--- my-project/components/Chat.jsx
+++ my-project/components/Chat.jsx
@@ -3,8 +3,44 @@ import React from 'react';
 const Chat = () => {
     return (
         <div>
-            <h1>Hello, World!</h1>
+           <div className='flex flex-col w-100 justify-between items-center bg-gray-200 p-4'>
+                <div className='h-20 bg-red-300 w-full'>
+                    menu
+                </div>
+                <div className='h-120 overflow-y-scroll w-full flex flex-col  gap-2 bg-red-400 p-2'>
+                    <div className='h-20 bg-amber-200 w-full shrink-0 rounded-lg'>
+                        h
+                    </div>
+                    <div className='h-20 bg-amber-200 w-full shrink-0 rounded-lg'>
+                        h
+                    </div>
+                    <div className='h-20 bg-amber-200 w-full shrink-0 rounded-lg'>
+                        h
+                    </div>
+                    <div className='h-20 bg-amber-200 w-full shrink-0 rounded-lg'>
+                        h
+                    </div>
+                    <div className='h-20 bg-amber-200 w-full shrink-0 rounded-lg'>
+                        h
+                    </div>
+                    <div className='h-20 bg-amber-200 w-full shrink-0 rounded-lg'>
+                        h
+                    </div>
+                    <div className='h-20 bg-amber-200 w-full shrink-0 rounded-lg'>
+                        h
+                    </div>
+                    <div className='h-20 bg-amber-200 w-full shrink-0 rounded-lg'>
+                        h
+                    </div>
+                    <div className='h-20 bg-amber-200 w-full shrink-0 rounded-lg'>
+                        h
+                    </div>
+              
+                 
+                </div>
+           </div>
         </div>
+        
     );
 };
 
